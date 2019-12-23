4f781f30c0b231fcca21f5774000f70d70ceb9a9
A-Breeze <alexander.breeze@yahoo.com>
2019-12-23 00:33:47 +0000

    add_pytest: Try to add a basic test and run with pytest, but "Unexpectedly quit" error is occurring.

---
+++ tests/test_simulate_data.py
@@ -0,0 +1,11 @@
+# from TWIST.simulate_data import SimulatedDataSet
+
+
+def test_generate_explanatory_data():
+    # Given
+
+    # When
+    # simulated_data = SimulatedDataSet()
+
+    # Then
+    assert False
